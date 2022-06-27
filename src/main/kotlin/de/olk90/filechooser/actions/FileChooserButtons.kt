fd5f12378053edf9b38fdfb5e582b32cdcbfbd4e
Oliver Köhler <olk.git@posteo.eu>
2022-06-27 09:33:27 +0200

    added icon parameter

--- src/main/kotlin/de/olk90/filechooser/actions/FileChooserButtons.kt
+++ src/main/kotlin/de/olk90/filechooser/actions/FileChooserButtons.kt
@@ -6,15 +6,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
-fun FileChooserButton(isDialogOpen: MutableState<Boolean>) {
+fun FileChooserButton(isDialogOpen: MutableState<Boolean>, icon: ImageVector = Icons.Filled.Home) {
     IconButton(
         onClick = {
             isDialogOpen.value = true
         }) {
         Icon(
-            Icons.Filled.Home,
+            icon,
             contentDescription = "Add new directory"
         )
     }
